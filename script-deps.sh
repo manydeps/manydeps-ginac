b13239a1a00c00d76df49273fe87e4968ae0a12b
Igor Machado <igor.machado@gmail.com>
2023-09-21 16:22:39 +0000

    fix: enforcing c++20 on cln

--- script-deps.sh
+++ script-deps.sh
@@ -41,6 +41,7 @@ $VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR install --triplet=$VCPKG_TRIPLET
 #echo "Applying PATCH"
 echo "ENFORCING C++20 on GINAC"
 cp tools/patch/ginac-CMakeLists.txt external/ginac/CMakeLists.txt
+cp tools/patch/cln-CMakeLists.txt external/cln/CMakeLists.txt
 
 echo "RUNNING cmake"
 mkdir -p build/
